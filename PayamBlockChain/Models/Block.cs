e9cc3516ae86678813762d01283ba3c8901dd544
Payam Shoghi <payam49er@gmail.com>
2019-06-03 22:11:32 -0400

    finished this part of work

--- PayamBlockChain/Models/Block.cs
+++ PayamBlockChain/Models/Block.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Security.Cryptography;
 using System.Text;
 using Clifton.Blockchain;
 using Newtonsoft.Json;
